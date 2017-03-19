4899322ec0be44a6c6c57d430119aeaecdc5ff6d
sagieppel <sagieppel@gmail.com>
2017-03-19 11:56:50 +0200

    Update Molecular dynamic with tensor flow.py

--- Molecular dynamic with tensor flow.py
+++ Molecular dynamic with tensor flow.py
@@ -1,5 +1,6 @@
 """
 Molecular Dynamic (M.D) with tensor flow: This code demonstrate how to build molecular dynamic simulation using tensorflow. 
+Used with python 3.5
 TensorFlow is an open source software library for numerical computation using data flow graph,
 It mostly used for machine learning but can be used to run any system as that can be written as dataflow graph. The code involve building data flow graph for single simulation step of molecular dynamics.
 This basically mean building the molecular dynamic simulation using tensor operations only. This this simulation step graph in runned in a loop to create multistep molecular dynamic model. The particles location is plotted on screen in each step.
